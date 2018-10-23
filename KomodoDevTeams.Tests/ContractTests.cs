30ab44e06a9bc9d095e9056fcc9946278ecca4e2
Joshua Tucker <thejltucker@gmail.com>
2018-10-23 14:04:05 -0400

    Fixed Naming Issue

--- KomodoDevTeams.Tests/ContractTests.cs
+++ KomodoDevTeams.Tests/ContractTests.cs
@@ -11,7 +11,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace KomodoDevTeams.Tests
 {
     [TestClass]
-    public class DevTests
+    public class ContractTests
     {
         private ContractController _controller;
         private MockContractService _mockService;
@@ -27,17 +27,24 @@ namespace KomodoDevTeams.Tests
         public void ContractController_PostContract_ShouldReturnOk()
         {
             _mockService.ReturnValue = true;
-            var contract = new ContractCreate {
-                ContractId = 1,
-                DevId = 1,
-                DevTeamId = 1,
-                EffectiveDate = DateTimeOffset.Now
-            };
+            var contract = new ContractCreate { ContractId = 1 };
 
             var result = _controller.Post(contract);
 
             Assert.IsInstanceOfType(result, typeof(OkResult));
             Assert.AreEqual(1, _mockService.CallCount);
+        }
+
+        [TestMethod]
+        public void ContractController_DeleteContract_ShouldReturnCorrectInt()
+        {
+            _mockService.ReturnValue = true;
+            _mockService.CallCount = 1;
+
+            var result = _controller.Delete(1);
+
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+            Assert.AreEqual(0, _mockService.CallCount);
         }
     }
 }
